88004f5b1d5646f21ce67116df8d44e1064cae3c
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-10-20 18:55:17 +0200

    Build Script V2

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+
+# Check the starting time (of the real build process)
+TIME_START=$(date +%s.%N)
 
 #Add CCACHE Options
 export USE_CCACHE=1
@@ -13,3 +29,13 @@ export ARCH=arm64
 make -j$BUILD_JOB_NUMBER -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android flash_gts3llte_defconfig
 make -j$BUILD_JOB_NUMBER -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
 cp out/arch/arm64/boot/Image $(pwd)/build/825/zImage
+
+cd $(pwd)/build/825
+zip -r9 Flash-KernelV0.1.zip * -x README Flash-KernelV0.1.zip
+
+# Check the finishing time
+TIME_END=$(date +%s.%N)
+
+# Log those times at the end as a fun fact of the day
+echo -e "${CLR_BLD_GRN}Total time elapsed:${CLR_RST} ${CLR_GRN}$(echo "($TIME_END - $TIME_START) / 60" | bc) minutes ($(echo "$TIME_END - $TIME_START" | bc) seconds)${CLR_RST}"
+echo -e ""
